813cc5c3331cc32da2228a84c0b654704a62fa1d
lhaggarty <lhaggarty@me.com>
2016-04-11 13:14:51 -0400

    update react layout js

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,25 +1,29 @@
 import React from "react";
 
-// import Header from "./Header";
-import Footer from "./Navbar";
+import Navbar from "./Navbar";
+import TopText from "./TopText";
+import Graph from "./Graph";
+import TestimonyCarousel from "./TestimonyCarousel";
+import Pricing from "./Pricing";
+import EmailSignUp from "./EmailSignUp";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      title: "Welcome",
-    };
-  }
-
-  changeTitle(title) {
-    this.setState({title});
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     title: "Welcome",
+  //   };
+  // }
+  //
+  // changeTitle(title) {
+  //   this.setState({title});
+  // }
 
   render() {
     return (
       <div>
-        // <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
+        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
         <navbar />
         <toptext />
         <graph />
